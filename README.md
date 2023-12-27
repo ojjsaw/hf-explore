a20bf2ef21918aa9096a58078f21ca4b3b32dffa
Ojas D. Sawant <ojjsaw@gmail.com>
2023-12-27 00:53:47 +0000

    initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+```
+python -m venv hf_ojas_explore
+source hf_ojas_explore/bin/activate
+```
+
+```
+python -m pip install --upgrade pip
+pip install --upgrade-strategy eager "optimum[openvino,nncf]"
+```
---
+++ test.py
@@ -0,0 +1,13 @@
+from optimum.intel import OVModelForCausalLM
+from transformers import AutoTokenizer, pipeline
+
+model_id = "helenai/gpt2-ov"
+model = OVModelForCausalLM.from_pretrained(model_id, local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+results = pipe("He's a dreadful magician and")
+print(results)
+
+
+
+
